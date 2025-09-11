1490dad54960829f51dc8d92c32ab097fd6471dd
namnguyen4106 <nguyennam4106@gmail.com>
2025-09-11 21:42:29 +0700

    first commit

---
+++ HW5/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    
+    private static void Main(string[] args)
+    {
+        MyDate md1 = new MyDate(DateTime.Now.Day,DateTime.Now.Month, DateTime.Now.Year);
+        Console.WriteLine("{0}/{1}/{2}",md1.Day,md1.Month,md1.Year);
+        Console.ReadKey();
+    }
+}
